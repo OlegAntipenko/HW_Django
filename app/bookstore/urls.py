ade2de5b2e7abf5b2b4557b48e99ff95a8775065
OlegAntipenko <aopz@i.ua>
2022-06-26 12:13:25 +0300

    django week 3

---
+++ app/bookstore/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from bookstore.views import *
+
+urlpatterns = [
+    path('books/books_list/', books_list, name="books"),
+    path('books/<int:index>/', book, name="book"),
+    path('books/author/<int:index>/', author, name="author"),
+    path('books/author_list/<int:index>/', author_list, name="author_list"),
+]
